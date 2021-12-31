4f3b87be8056d853c10340160bdb2b2f859c13ee
Valtercio Santiago Ferreira Junior <juniorvaltercio2000@gmail.com>
2021-12-31 16:39:53 -0300

    consultando api

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-const conversor = ()=>{
+urlDolar = ' https://economia.awesomeapi.com.br/last/USD-BRL'
+urlEuro = 'https://economia.awesomeapi.com.br/last/EUR-BRL'
+
+const conversor = async ()=>{
     const moedas = document.querySelectorAll("#moedas option")
     
     let valor = document.getElementById("valor").value
     let result = document.getElementById("result")
   
-    moedas.forEach(e => {
+     const responseDolar = await fetch(urlDolar)
+     const cotacaoDolar = await responseDolar.json()
+      const responseEuro = await fetch(urlEuro)
+     const cotacaoEuro = await responseEuro.json()
+      
+     moedas.forEach(e => {
         if(e.selected == true && e.value=='Dolar'){
-            result.innerText = `${valor} $ = ${(valor*5.74).toFixed(2)} BRL`
+            result.innerText = `${valor} $ = ${(valor*parseFloat(cotacaoDolar.USDBRL.low
+)).toFixed(2)} BRL`
         }
         else if (e.selected == true && e.value=='Euro'){
-            result.innerText = `${valor} € = ${(valor*6.47).toFixed(2)} BRL`
+            result.innerText = `${valor} € = ${(valor*parseFloat(cotacaoEuro.EURBRL.low
+)).toFixed(2)} BRL`
         }
     })
   }
-  
   
