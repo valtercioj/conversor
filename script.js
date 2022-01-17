921a4ee7ffe304d014584f9f9ea850c4f198e95e
Valtercio Santiago Ferreira Junior <juniorvaltercio2000@gmail.com>
2022-01-17 19:59:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ const conversor = async ()=>{
   
      const responseDolar = await fetch(urlDolar)
      const cotacaoDolar = await responseDolar.json()
-      const responseEuro = await fetch(urlEuro)
+     const responseEuro = await fetch(urlEuro)
      const cotacaoEuro = await responseEuro.json()
       
      moedas.forEach(e => {
         if(e.selected == true && e.value=='Dolar'){
-            result.innerText = `${valor} $ = ${(valor*parseFloat(cotacaoDolar.USDBRL.low
+            result.innerText = `${valor} $ = ${(valor*parseFloat(cotacaoDolar.USDBRL.high
 )).toFixed(2)} BRL`
         }
         else if (e.selected == true && e.value=='Euro'){
-            result.innerText = `${valor} € = ${(valor*parseFloat(cotacaoEuro.EURBRL.low
+            result.innerText = `${valor} € = ${(valor*parseFloat(cotacaoEuro.EURBRL.high
 )).toFixed(2)} BRL`
         }
     })
